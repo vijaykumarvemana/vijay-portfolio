c744fe4220047cc6a58833d98fe87f4400cb0655
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2022-02-01 18:12:31 +0100

    updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -48,14 +48,14 @@ const Projects = () => {
                         
                         </div>
                         <p className='mt-2'>Skills:</p>
-                        <div className='mb-2'><span className='tech'>React</span>
-                        <span className='tech'>Redux</span>
+                        <div className='mb-2'><span className='tech'>HTML5</span>
+                        <span className='tech'>CSS3</span>
                         <span className='tech'>Node</span>
-                        <span className='tech'>Express</span><br/></div>
+                        <span className='tech'>D3js</span><br/></div>
                          <div> 
-                             <span className='tech my-1'>MongoDB</span>
-                             <span className='tech'>Recharts</span>
-                             <span className='tech'>MUI</span>
+                             <span className='tech my-1'>Charts</span>
+                             {/* <span className='tech'>Recharts</span> */}
+                             {/* <span className='tech'>MUI</span> */} 
                          </div>
                        
                        
@@ -72,23 +72,27 @@ const Projects = () => {
                         <div className="img-hover-zoom img-hover-zoom--colorize"><img src={project2} /> 
                         
                         </div>
-                        <p className='my-2 mb-3'>Skills:</p>
-                        <div className='my-3'><span className='tech'>HTML5</span>
+                        <p className='mt-2'>Skills:</p>
+                        <div className='mb-2'><span className='tech'>HTML5</span>
                         <span className='tech'>CSS3</span>
-                        <span className='tech mb-4'>JavaScript</span>
-                        
+                        <span className='tech'>JavaScript</span>
+                        {/* <span className='tech'></span> */}
+                        <br/></div>
+                         <div> 
+                             <span className='tech my-1'>Animations</span>
+                             {/* <span className='tech'></span>
+                             <span className='tech'></span>  */}
+                         </div>
                        
-                       </div>
-                       
                         
-                        <div className='project-links my-5'>
+                        <div className='project-links '>
                         <a href='https://travel-six-eta.vercel.app/' target="_blank"><Button className='links'> <span className="mx-2 "><FiLink /></span>Website</Button></a>
                         <a href='https://github.com/vijaykumarvemana/travel' target="_blank"><Button className='links' > <span className="mx-2 "><FiGithub /></span>GitHub</Button></a>
                         </div>
                     </div>
                     </Col>
                 </Row>
-                <Row md={4} className="project-row" >
+                {/* <Row md={4} className="project-row" >
                     
                     <Col className="project-col mt-5">
                     <div className='project-container'>
@@ -166,10 +170,10 @@ const Projects = () => {
                         </div>
                     </div>
                     </Col>
-                </Row>
+                </Row> */}
                 
                 </Container>
-                <h6 className='text-center py-5  text-primary git-repos'><a href="https://github.com/vijaykumarvemana?tab=repositories" target="_blank">Repositories on GitHub</a></h6>
+                <h6 className='text-center py-5  git-repos'><a className="git-link" href="https://github.com/vijaykumarvemana?tab=repositories" target="_blank">Repositories on GitHub</a></h6>
                 </section>
       </Element>
     )
